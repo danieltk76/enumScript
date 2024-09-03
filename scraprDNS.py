59ec4a02e1893af1baa8a703a6f844ae3b87a0de
danieltk76 <knight.daniel0612@gmail.com>
2024-09-02 21:45:55 -0400

    setup structure for scraping viewdns.info

--- scraprDNS.py
+++ scraprDNS.py
@@ -1,6 +1,17 @@
 #script aspect to scrape ViewDNS.info
+from selenium import webdriver
+from selenium.webdriver.comon.keys import Keys
 
-from bs4 import BeautifulSoup
+driver = webdriver.Chrome(executeable_path='')
+driver.get('https://viewdns.info')
 
-url = "https://viewdns.info/"
-ip = ""
+command_box = driver.find_element_by_id()
+command_box.send_keys(input)
+command_box.send_keys(Keys.RETURN)
+
+driver.implicitly_wait(5)
+
+result = driver.find_element_by_id('resultBox')
+print(result.text)
+
+driver.quit()
