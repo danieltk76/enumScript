4eb8b5227d131fdc227058ae4ca3a923beef2970
danieltk76 <knight.daniel0612@gmail.com>
2024-09-04 16:45:32 -0400

    reworking and establishing commands

--- htmlstatus.py
+++ htmlstatus.py
@@ -2,35 +2,38 @@
 import requests
 import sys
 
-
-
 run = True
 while run: 
-    target = input("Enter target url: ")
-    try:
-        response = requests.get(target)
-        status = response.status_code
+    # we need to establish some key commandss
+    entry_command = input("Welcome to our recon tool. enter -h for help! ")
+    if entry_command == "-h":
+        print("""I hope these help: 
+            -h                     --> help menu
+            scout http://target ip --> gain server code recon on target
+            dnsRec <target ip>     --> use View.DNS through the shell, currently expanding""")
+  
+        # response = requests.get(target)
+        # status = response.status_code
 
-        if len(sys.argv) != target:
-            print("Incorrect usage, enter target IP to get http status")
-            sys.exit(1)
-        else:
-            for status in range(100, 511):
-                if 200 <= status <= 226:
-                    print("slow processing: ", status)
-                elif 200 <= status <= 226:
-                    print("It is live:", status)
-                elif 300 <= status <= 308:
-                    print("website redirect, check nano file:", status)
-                elif 400 <= status <= 451:
-                    print("Client error response: ", status)
-                elif 500 <= status <= 511:
-                    print("likely server error: ", status)
+        # if len(sys.argv) != target:
+        #    print("Incorrect usage, enter target IP to get http status")
+        #    sys.exit(1)
+        # else:
+        #    for status in range(100, 511):
+        #        if 200 <= status <= 226:
+        #           print("slow processing: ", status)
+        #        elif 200 <= status <= 226:
+        #            print("It is live:", status)
+        #        elif 300 <= status <= 308:
+        #            print("website redirect, check nano file:", status)
+        #        elif 400 <= status <= 451:
+        #            print("Client error response: ", status)
+        #        elif 500 <= status <= 511:
+        #            print("likely server error: ", status)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
 
-    exit_input = input("Do you want to check another URL? (Y/N)").strip().lower()
-    if exit_input != 'yes':
-        run = False
+    #exit_input = input("Do you want to check another URL? (Y/N)").strip().lower()
+    #if exit_input != 'yes':
+    #   run = False
+        
             
