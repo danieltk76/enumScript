11e9f3347fb3418781257f72d02df2711ae3eb20
danieltk76 <knight.daniel0612@gmail.com>
2024-09-04 17:07:52 -0400

    fixed http code problem

--- InfoCreep.py
+++ InfoCreep.py
@@ -19,41 +19,31 @@ print("""     ,%&& %&& %
       |;:  |(____.-'     '.   ~   -    `    ~
       |;:  |  \ / `\       //.  -    ^   ~
       |;:  |\ /' /\_\_        ~. _ ~   -   //-
- jgs\\/;:   \'--' `---`           `\\//-\\///""")
+    \\/;:   \'--' `---`           `\\//-\\///""")
 
 run = True
 while run: 
-    # we need to establish some key commands
-    
-    entry_command = input("Welcome to InfoCreep. Enter -h for help! ")
-    if entry_command == "-h":
+    # we need to establish some key commands...and some ascii art
+
+    command = input("Welcome to InfoCreep. Enter -h for help! ")
+    if command == "-h":
         print("""I hope these help: 
             -h                     --> help menu
             scout http://target ip --> gain server code recon on target
             dnsRec <target ip>     --> use View.DNS through the shell, currently expanding""")
-  
-        # response = requests.get(target)
-        # status = response.status_code
+    elif command.startswith("scout "):
+        target_url = command.split("scout ")[1]
 
-        # if len(sys.argv) != target:
-        #    print("Incorrect usage, enter target IP to get http status")
-        #    sys.exit(1)
-        # else:
-        #    for status in range(100, 511):
-        #        if 200 <= status <= 226:
-        #           print("slow processing: ", status)
-        #        elif 200 <= status <= 226:
-        #            print("It is live:", status)
-        #        elif 300 <= status <= 308:
-        #            print("website redirect, check nano file:", status)
-        #        elif 400 <= status <= 451:
-        #            print("Client error response: ", status)
-        #        elif 500 <= status <= 511:
-        #            print("likely server error: ", status)
+        try:
+            r = requests.get(target_url)
+            print(f"Status Code for {target_url}: {r.status_code}")
 
-
-    #exit_input = input("Do you want to check another URL? (Y/N)").strip().lower()
-    #if exit_input != 'yes':
-    #   run = False
+        except requests.exceptions.RequestException as e:
+            # Handle any request-related exceptions
+            print(f"Error connecting to {target_url}: {e}")
+    
+    elif command.lower() == "exit":
+        print("Exiting InfoCreep...")
+        break
         
             
